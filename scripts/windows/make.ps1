58d19736520d9a34f8072fa24f630fe36a7b0a54
Jeff Cheng <jcheng@signalfx.com>
2019-03-22 17:01:05 -0400

    Sign executable if pfx and password exists

--- scripts/windows/make.ps1
+++ scripts/windows/make.ps1
@@ -24,7 +24,7 @@ function signalfx-agent([string]$AGENT_VERSION="", [string]$AGENT_BIN=".\signalf
     versions_go
 
     go build -mod vendor -o "$AGENT_BIN" github.com/signalfx/signalfx-agent/cmd/agent
-    if ($lastexitcode -ne 0){ exit $lastexitcode }
+    if ($lastexitcode -ne 0){ throw }
 }
 
 # make the build bundle
@@ -38,6 +38,8 @@ function bundle (
         [bool]$DOWNLOAD_COLLECTD_PLUGINS=$false,
         [bool]$ZIP_BUNDLE=$true,
         [bool]$ONLY_BUILD_AGENT=$false,
+        [string]$PFX_PATH="",
+        [string]$PFX_PASSWORD="",
         [string]$AGENT_NAME="SignalFxAgent") {
 
     if ($AGENT_VERSION -Eq ""){
@@ -53,6 +55,17 @@ function bundle (
     if ($BUILD_AGENT) {
         Remove-Item -Recurse -Force "$buildDir\$AGENT_NAME\bin\signalfx-agent.exe" -ErrorAction Ignore
         signalfx-agent -AGENT_VERSION "$AGENT_VERSION" -AGENT_BIN "$buildDir\$AGENT_NAME\bin\signalfx-agent.exe"
+    }
+
+    if ($PFX_PATH -ne "") {
+        if (!(Test-Path -Path "$PFX_PATH")) {
+            throw "$PFX_PATH does not exist!"
+        }
+        if ($PFX_PASSWORD -eq "") {
+            throw "PFX_PASSWORD is required for $PFX_PATH"
+        }
+        signtool sign /f "$PFX_PATH" /p $PFX_PASSWORD /tr http://timestamp.digicert.com /fd sha256 /td SHA256 /n "SignalFx, Inc." "$buildDir\$AGENT_NAME\bin\signalfx-agent.exe"
+        if ($lastexitcode -ne 0){ throw }
     }
 
     if (($DOWNLOAD_PYTHON -Or !(Test-Path -Path "$buildDir\python")) -And !$ONLY_BUILD_AGENT) {
